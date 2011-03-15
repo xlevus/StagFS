95779bc300f09f3f0fa9b7b3717d49331d2092ea
Chris Targett <xin@xlevus.net>
2011-03-15 23:42:35 +1300

    More or less working inotify code

--- stag/data.py
+++ stag/data.py
@@ -32,20 +32,29 @@ class DataManager(threading.Thread):
     def __init__(self, db_name, source_folders, loaders):
         super(DataManager, self).__init__()
 
-        self.db_name = db_name
-        self.source_folders = map(os.path.abspath, source_folders)
         self.loaders = loaders
+        self.db_name = os.path.abspath(db_name)
+        self.source_folders = map(os.path.abspath, source_folders)
+ 
+    def load_initial(self):
+        conn = stag.db.ConnectionWrapper(self.db_name)
+        logger.debug("Clearing stagfs table")
+        conn.execute("DELETE FROM stagfs WHERE 1")
+
+        for dir in self.source_folders:
+            self.process_dir(dir, conn)
 
     def start(self, exit_event):
         self.exit_event = exit_event
         self.fd = inotifyx.init()
-        self.wd_list = []
+        self.wd_list = {}
 
-        events = inotifyx.IN_CREATE | inotifyx.IN_DELETE | inotifyx.IN_MODIFY
+        events = inotifyx.IN_CREATE | inotifyx.IN_DELETE | inotifyx.IN_MODIFY | \
+                inotifyx.IN_MOVED_TO | inotifyx.IN_MOVED_FROM
         for path in self.source_folders:
             path = os.path.abspath(path)
             try:
-                self.wd_list.append(inotifyx.add_watch(self.fd, os.path.abspath(path), events))
+                self.wd_list[inotifyx.add_watch(self.fd, path, events)] = path
             except Exception, e:
                 logger.error("Failed to setup inotify watch on '%s'. Reason: %r" % (path, e))
 
@@ -57,26 +66,51 @@ class DataManager(threading.Thread):
             # Instead use a short timeout on get_events and sleep afterwards
             events = inotifyx.get_events(self.fd, 0.1)
             for e in events:
-                logger.debug("Inotify Event: %r" % e)
+                try:
+                    e.name = os.path.join(self.wd_list[e.wd], e.name) # Normalise event name
+                    logger.debug("Inotify Event: %r" % e)
+                    if e.mask & (inotifyx.IN_DELETE | inotifyx.IN_MOVED_FROM | inotifyx.IN_MODIFY):
+                        self.handle_delete(e)
+                    if e.mask & (inotifyx.IN_CREATE | inotifyx.IN_MOVED_TO | inotifyx.IN_MODIFY):
+                        self.handle_create(e)
+                except Exception, e:
+                    logger.error("Thread Error: %r" % e)
+
             time.sleep(5)
        
-        for wd in self.wd_list:
+        for wd in self.wd_list.keys():
             rm_watch(self.fd, wd)
         os.close(self.fd)
     
-    def load_initial(self):
-        conn = stag.db.ConnectionWrapper(self.db_name)
-        logger.debug("Clearing stagfs table")
-        conn.execute("DELETE FROM stagfs WHERE 1")
+    def process_dir(self, dir, conn=None):
+        for root, dirs, files in os.walk(dir):
+            for f in files:
+                self.process_file(os.path.join(root, f), conn)
 
-        for dir in self.source_folders:
-            os.path.walk(dir, self.process_folder, conn)
-     
-    def process_folder(self, conn, directory, contents):
-        func = stag.utils.curry(os.path.join, directory)
-        for source_file in map(func, contents):
-            for extension, loader in self.loaders:
-                if source_file.endswith("."+extension):
-                    logger.debug("Found %r. Loading with %r" % (source_file, loader))
-                    loader(conn, source_file)
-       
+    def process_file(self, source_file, conn=None):
+        for extension, loader in self.loaders:
+            if source_file.endswith("."+extension):
+                logger.debug("Found %r. Loading with %r" % (source_file, loader))
+
+                if conn is None:
+                    conn = stag.db.ConnectionWrapper(self.db_name)
+
+                loader(conn, source_file)
+
+    def process_path(self, path):
+        """Utility function to process either a folder or a directory"""
+        if os.path.isfile(path):
+            self.process_file(path)
+        elif os.path.isdir(path):
+            self.process_dir(path)
+      
+    def handle_delete(self, event):
+        conn = stag.db.ConnectionWrapper(self.db_name)
+        conn.execute(
+            "DELETE FROM stagfs WHERE source = ?",
+            (event.name,)
+        )
+
+    def handle_create(self, event):
+        self.process_path(event.name)
+
